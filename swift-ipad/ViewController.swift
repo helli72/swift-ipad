0d8bbc15a08ac0b9b1a8a817eb8a2d1dcc53a147
Helmut Schmid <helmut_schmid@gmx.de>
2014-10-18 12:08:34 +0200

    Initial commit

---
+++ swift-ipad/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  swift-ipad
+//
+//  Created by swift-ipad on 18.10.14.
+//  Copyright (c) 2014 swift-ipad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outputText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        outputText.text = "Hello Swift"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
